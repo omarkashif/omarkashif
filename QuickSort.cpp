59cda36da1f3fbbf145f7766aa9b852ccb83d4d8
omarkashif <64086493+omarkashif@users.noreply.github.com>
2022-09-24 11:34:09 +0500

    Add files via upload

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,35 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-void QuickSort(int arr[], int s, int n)
+void QuickSort(int *arr, int s, int n)
 {
-    if(s < n)
+    if (s < n)
     {
-    int piv = arr[s], i = s;
-    int j = n;
+        int piv = arr[s], i = s;
+        int j = n;
 
-    while (i < j)
-    {
-        while (arr[i] <= piv)
+        while (i < j)
         {
-            i++;
+            while (arr[i] <= piv)
+            {
+                i++;
+            }
+            while (arr[j] > piv)
+            {
+                j--;
+            }
+            if (i < j)
+                swap(arr[i], arr[j]);
         }
-        while (arr[j] > piv)
-        {
-            j--;
-        }
-        if (i < j)
-            swap(arr[i], arr[j]);
+        swap(arr[j], arr[s]);
+        piv = j;
+        QuickSort(arr, s, j - 1);
+        QuickSort(arr, j + 1, n);
     }
-    swap(arr[j], arr[s]);
-    piv = j;
-    QuickSort(arr, s, j-1);
-    QuickSort(arr, j + 1, n);}
 }
 int main()
 {
-    int arr[5] = {12, 11, 13, 5, 6};
-    QuickSort(arr, 0, 5-1);
-    for (int k = 0; k < 5; k++)
+    int x;
+    cin >> x;
+    int *arr = new int[x];
+    for (int i = 0; i < x; i++)
+        cin >> arr[i];
+    QuickSort(arr, 0, x - 1);
+    for (int k = 0; k < x; k++)
     {
         cout << arr[k] << "\n";
     }
