ad7821204c37887377dab38167a3e40aeb1bb5f8
omarkashif <64086493+omarkashif@users.noreply.github.com>
2022-09-25 17:03:57 +0500

    Add files via upload

---
+++ RadixSort.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxdig(int *arr, int n)
+{
+    int max = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[0] > max)
+            max = arr[0];
+    }
+    int d = 0;
+    while (max)
+    {
+        max = max / 10;
+        d++;
+    }
+    return d;
+}
+int pow10(int n)
+{
+    int a = 1;
+    for (int i = 0; i < n; i++)
+    {
+        a = a * 10;
+    }
+    return a;
+}
+void RadixSort(int arr[], int n)
+{
+    int d = maxdig(arr, n);
+    int z = 0;
+    while (d)
+    {
+        d--;
+        vector<int> r[10];
+        for (int i = 0; i < n; i++)
+        {
+            r[((arr[i] / pow10(z)) % 10)].push_back(arr[i]);
+        }
+        int index = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < (r[i].size()); j++)
+            {
+                arr[index++] = r[i][j];
+            }
+        }
+        z++;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    RadixSort(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
