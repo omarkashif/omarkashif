e2f1a82a878ffa54db404029fdc77ba69be2b341
omarkashif <64086493+omarkashif@users.noreply.github.com>
2022-12-15 16:32:48 +0500

    Update RadixSort.cpp

--- RadixSort.cpp
+++ RadixSort.cpp
@@ -48,15 +48,3 @@ void RadixSort(int arr[], int n)
         z++;
     }
 }
-int main()
-{
-    int n;
-    cin >> n;
-    int *arr = new int[n];
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-    RadixSort(arr, n);
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    return 0;
-}
